72180f62e49dfe75b7c55f0d16164190b2069357
IlyaM70 <iliya.merkulov@mail.ru>
2023-09-24 17:40:49 +0500

    Db initilized

---
+++ RentApartmentsAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentApartmentsAPI.Models;
+
+namespace RentApartmentsAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        public DbSet<Apartment> Apartments { get; set; }
+    }
+}
--- RentApartmentsAPI/Program.cs
+++ RentApartmentsAPI/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RentApartmentsAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlConnection"))
+    );
 builder.Services.AddControllers()
     .AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
